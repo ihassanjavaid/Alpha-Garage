51b9840c4f0836e8299b0d2fc1365253d800e7e4
ihassanjavaid <great.maliks@gmail.com>
2020-04-04 00:01:56 +0500

    update

--- lib/components/addContactDialog.dart
+++ lib/components/addContactDialog.dart
@@ -5,8 +5,7 @@ import 'package:alphagarage/services/firestore_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AddContact{
-
+class ContactDialog {
   // Data Attributes
   String email;
   String password;
@@ -14,130 +13,126 @@ class AddContact{
 
   Auth _auth = Auth();
 
-  String removeSpaces(String email) => email.replaceAll(' ','');
+  String removeSpaces(String email) => email.replaceAll(' ', '');
 
-  addContact(context){
+  addContact(context) {
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return Dialog(
             shape: RoundedRectangleBorder(
-                borderRadius:
-                BorderRadius.circular(5.0),
+              borderRadius: BorderRadius.circular(5.0),
             ),
-            child: signUpCard(context),
+            child: _signUpCard(context),
           );
         });
   }
 
-  Widget signUpCard(BuildContext context) {
-
+  Widget _signUpCard(BuildContext context) {
     return Container(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        elevation: 12,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Add User",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 28,
-                      fontWeight: FontWeight.w600,
-                    ),
+      height: 430,
+      //width: 20,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Add User",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: CustomTextField(
-                  placeholder: 'Your Name',
-                  onChanged: (value) {
-                    this.displayName = value;
-                  },
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: CustomTextField(
+                placeholder: 'Your Name',
+                onChanged: (value) {
+                  this.displayName = value;
+                },
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: CustomTextField(
-                  placeholder: 'Your Email',
-                  onChanged: (value) {
-                    this.email = value;
-                  },
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: CustomTextField(
+                placeholder: 'Your Email',
+                onChanged: (value) {
+                  this.email = value;
+                },
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: CustomTextField(
-                  placeholder: 'Your Password',
-                  isPassword: true,
-                  onChanged: (value) {
-                    this.password = value;
-                  },
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: CustomTextField(
+                placeholder: 'Your Password',
+                isPassword: true,
+                onChanged: (value) {
+                  this.password = value;
+                },
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-                child: Text(
-                  "Password must be at least 8 characters and include a special character and number",
-                  style: TextStyle(color: Colors.grey),
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+              child: Text(
+                "Password must be at least 8 characters and include a special character and number",
+                style: TextStyle(color: Colors.grey),
               ),
-              ButtonTheme(
-                minWidth: double.maxFinite,
-                child: FlatButton(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text("Add User",
+            ),
+            ButtonTheme(
+              minWidth: double.maxFinite,
+              child: FlatButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Add User",
                       style: TextStyle(
                         fontSize: 17,
                       ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                        child: Icon(
-                          Icons.person_add
-                        ),
-                      )
-                    ],
-                  ),
-                  color: Colors.brown,
-                  textColor: Colors.white,
-                  padding: EdgeInsets.only(
-                      left: 38, right: 38, top: 12, bottom: 12),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  onPressed: () async {
-                    try {
-                      await _auth.registerUser(
-                          email: removeSpaces(this.email), password: this.password);
-                      await _auth.updateUserInfo(
-                          displayName: this.displayName);
-                      await FirestoreService().registerUser(
-                          email: removeSpaces(this.email), displayName: this.displayName);
-                      //Navigator.popAndPushNamed(context, Index.id);
-                      Navigator.pop(context);
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: Icon(Icons.person_add),
+                    )
+                  ],
                 ),
+                color: Colors.brown,
+                textColor: Colors.white,
+                padding:
+                    EdgeInsets.only(left: 38, right: 38, top: 12, bottom: 12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5)),
+                onPressed: () async {
+                  try {
+                    await _auth.registerUser(
+                        email: removeSpaces(this.email),
+                        password: this.password);
+                    await _auth.updateUserInfo(displayName: this.displayName);
+                    await FirestoreService().registerUser(
+                        email: removeSpaces(this.email),
+                        displayName: this.displayName);
+                    //Navigator.popAndPushNamed(context, Index.id);
+                    Navigator.pop(context);
+                  } catch (e) {
+                    print(e);
+                    // just for now TODO remove this
+                    Navigator.pop(context);
+                  }
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
-
 }
--- lib/screens/contacts.dart
+++ lib/screens/contacts.dart
@@ -6,6 +6,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:alphagarage/services/firestore_service.dart';
 import 'package:alphagarage/utilities/userData.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Contacts extends StatelessWidget {
   static const String id = 'contacts_screen';
@@ -29,15 +31,47 @@ class Contacts extends StatelessWidget {
       body: Center(
         child: SwipeList(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // TODO add contact
-          AddContact().addContact(context);
-        },
-        child: Icon(Icons.person_add),
+      floatingActionButton: SpeedDial(
+          marginRight: 18,
+          marginBottom: 20,
+          animatedIcon: AnimatedIcons.menu_close,
+          animatedIconTheme: IconThemeData(size: 29.0),
+        visible: true,
+        closeManually: true,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.white,
+        overlayOpacity: 1,
+        //onOpen: () => print('OPENING DIAL'),
+        //onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
         backgroundColor: Colors.brown,
+        foregroundColor: Colors.white,
+        elevation: 10.0,
+        shape: CircleBorder(),
+        children: [
+          SpeedDialChild(
+              child: Icon(FontAwesomeIcons.doorOpen),
+              backgroundColor: Colors.red,
+              label: 'Sign out',
+              labelStyle: TextStyle(fontSize: 18.0),
+              onTap: () {
+                // TODO Add Sign out functionality
+              },
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.person_add),
+            backgroundColor: Colors.green,
+            label: 'Add User',
+            labelStyle: TextStyle(fontSize: 18.0),
+            onTap: () {
+              ContactDialog().addContact(context);
+            },
+          ),
+        ],
       ),
-    );
+
+      );
   }
 }
 
