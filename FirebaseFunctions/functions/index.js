64a54c6ccc397abe41d06dccc5822990f95dd73f
Elvin-Arrow <30791291+Elvin-Arrow@users.noreply.github.com>
2020-04-04 19:01:11 +0500

    Notification integration initialized

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -13,10 +13,21 @@ exports.notificationTrigger = functions.firestore.document('messages/{messageId}
 
     newData = snapshot.data;
 
+    var tokens = [
+        'cBWf6VujWFQ:APA91bGys4eRXhW3iZQB0b97oaj-gZzC-kHalgPNGEPtqXKUCnISzJBjP7CIodChipiQmgWPup_z6gy7k_Z0tEJxZTZ-y3SEgpIe6TtOoF8TZBiKncCF6eJ0peWzZ-AsaUa7qWvnCSio'
+    ];
+
     var payload = {
         notification : {title: 'Push Title', body: 'Push body', sound: 'default'},
-        data: {message: 'Push notification message'}
+        data: {click_action: 'FLUTTER_NOTIFICATION_CLICK' , message: 'Push notification message'}
     };
+
+    try {
+        const response = await admin.messaging().sendToDevice(tokens, payload);
+        console.log('Notification sent sucessfully.');
+    } catch (err) {
+        console.log('Error sending notification.');
+    }
 
 });
 
