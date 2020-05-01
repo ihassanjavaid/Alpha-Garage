a5c75a1cd1ea1560a30b9d0c4b5522f4e5d21599
elvin_arrow <m.talhabinmansoor@gmail.com>
2020-05-02 01:08:35 +0500

    Update

--- lib/screens/conversation_screen.dart
+++ lib/screens/conversation_screen.dart
@@ -77,42 +77,6 @@ class ConversationScreen extends StatelessWidget {
                 ),
               ),
             ),
-            /*StreamBuilder<QuerySnapshot>(
-              stream:
-                  FirestoreService().firestore.collection('chats').snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                final messages = snapshot.data.documents.reversed;
-                List<MessageBubble> messageBubbles = [];
-                for (var message in messages) {
-                  final messageText = message['messageText'];
-                  final messageSender = message['messageSender'];
-                  final timestamp = message['timestamp'];
-
-                  final MessageBubble messageBubble = MessageBubble(
-                    messageText: messageText,
-                    isMe: widget.user.email == messageSender,
-                    timestamp: timestamp,
-                  );
-                  messageBubbles.add(messageBubble);
-                }
-
-                return Expanded(
-                  child: ListView(
-                    reverse: true,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                      vertical: 20.0,
-                    ),
-                    children: messageBubbles,
-                  ),
-                );
-              },
-            )*/
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -137,6 +101,7 @@ class ConversationScreen extends StatelessWidget {
                         messageText: _message,
                         timestamp: DateTime.now().millisecondsSinceEpoch,
                       );
+                      
                     },
                     child:
                         /*Text(
